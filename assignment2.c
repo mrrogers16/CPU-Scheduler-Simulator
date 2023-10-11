2e6f330e328fae98ecbd0b3091dac388acd5e220
mclarke16 <mick.clarke@hotmail.com>
2023-10-11 09:48:49 -0500

    updates

--- assignment2.c
+++ assignment2.c
@@ -1,21 +1,32 @@
 #include <stdio.h>
-
+/*
+pslibrary.c
+pslibrary.h
+assign2.c
+runall.c
+makefile
+*/
+void caller()
+{
+    s1[x1 + y1 + z1 + x2 + y2 + z2];
+}
 int main(int argc, char *argv[])
 {
     int i;
 
     if (argc < 7)
     {
-        fprintf(stderr, "ERROR USAGE: file1 file2 ... file n\n", argv[0]);
+        fprintf(stderr, "ERROR USAGE: file1 file2 ... file n\n");
         return 1;
     }
     else
     {
-        printf("CS 3733 / Assignment 2 / written by Mickey Clarke / hpz729");
-
-        for(i = 0; i < argc; i++)
+        printf("CS 3733 / Assignment 2 / written by Mickey Clarke / hpz729\n");
+        printf("Inputs: ");
+        for(i = 1; i < argc; i++)
         {
-            printf("Inputs: %d", argv[i]);
+            printf("%s ", argv[i]);
         }
+        printf("\n");
     }
 }
