69ee83d094259cf298c238b14d5ffe5d912f282a
mclarke16 <mick.clarke@hotmail.com>
2023-10-16 16:44:20 -0500

    Possible psjf implemented

--- pslibrary.c
+++ pslibrary.c
@@ -19,6 +19,96 @@ avoid putting in multiple string terminators
 /* assume s1 and s2 point to buffers with enough space to hold the result */
 /* assume that the int parameters are strictly greater than 0 */
 
+void psjf(char *s1, char *s2, int x1, int y1, int z1,
+          int x2, int y2, int z2)
+{
+    int i;              /* next string position (time) */
+    int state1 = READY; /* start with both ready */
+    int state2 = READY;
+    int cpuLeft1 = x1; /* P1 next CPU burst remaining */
+    int cpuLeft2 = x2; /* P2 next CPU burst remaining */
+    int ioLeft1 = y1;  /* P1 next IO burst remaining, 0 if no more IO */
+    int ioLeft2 = y2;  /* P2 next IO burst remaining, 0 if no more IO */
+
+    for (i = 0; (state1 != DONE) || (state2 != DONE); i++)
+    {
+        /* running process completes its CPU burst */
+        if ((state1 == RUNNING) && (cpuLeft1 == 0))
+        {
+            if (ioLeft1 == 0)
+            {
+                state1 = DONE;
+                s1[i] = stateChars[state1]; /* terminate the string */
+            }
+            else
+                state1 = WAITING;
+        }
+        else if ((state2 == RUNNING) && (cpuLeft2 == 0))
+        {
+            if (ioLeft2 == 0)
+            {
+                state2 = DONE;
+                s2[i] = stateChars[state2]; /* terminate the string */
+            }
+            else
+                state2 = WAITING;
+        }
+        /* handle IO complete */
+        if ((state1 == WAITING) && (ioLeft1 == 0))
+        {
+            state1 = READY;
+            cpuLeft1 = z1;
+        }
+        if ((state2 == WAITING) && (ioLeft2 == 0))
+        {
+            state2 = READY;
+            cpuLeft2 = z2;
+        }
+        /* if both ready, depends on algorithm */
+        if ((state1 == READY) && (state2 == READY))
+        {
+            if(cpuLeft1 > cpuLeft2)
+            {
+                state2 = RUNNING;
+            }
+            else if(cpuLeft1 < cpuLeft2)
+            {
+                state1 = RUNNING;
+            }
+            else
+            {
+                state1 = RUNNING; /* Default tie breaker to lowest PID*/
+            }
+        }
+        /* handle one ready and CPU available */
+        else if ((state1 == READY) && (state2 != RUNNING))
+        {
+            state1 = RUNNING;
+        }
+        else if ((state2 == READY) && (state1 != RUNNING))
+        {
+            state2 = RUNNING;
+        }
+        /* insert chars in string, but avoid putting in extra string terminators */
+        if (state1 != DONE)
+            s1[i] = stateChars[state1];
+        if (state2 != DONE)
+            s2[i] = stateChars[state2];
+        /* decrement counts */
+        if (state1 == RUNNING)
+            cpuLeft1--;
+        if (state1 == WAITING)
+            ioLeft1--;
+        if (state2 == RUNNING)
+            cpuLeft2--;
+        if (state2 == WAITING)
+            ioLeft2--;
+    } /* end of main for loop */
+}
+
+
+
+
 void sjf(char *s1, char *s2, int x1, int y1, int z1,
          int x2, int y2, int z2)
 {
